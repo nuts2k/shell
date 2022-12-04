b712018deae6a69517cb10996a93e0dce3befa6c
nuts2k <95838042+nuts2k@users.noreply.github.com>
2022-12-04 15:00:57 +0800

    Create pushtg.sh

---
+++ pushtg.sh
@@ -0,0 +1,15 @@
+if [ "$#" -eq 0 ]
+then
+        echo 'message required'
+        exit 0
+fi
+
+
+TOKEN=xxxxxx   #TG机器人token
+chat_ID=xxxxxx              #用户ID或频道、群ID
+message_text=$*         #要发送的信息
+MODE='HTML'             #解析模式，可选HTML或Markdown
+URL="https://api.telegram.org/bot${TOKEN}/sendMessage"          #api接口
+#测试2：终端无日志
+curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d text="${message_text}"
+#echo $message_text
