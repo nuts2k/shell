12c4242ced8e9f13f5f7d7c52fde45e82a05d78f
nuts2k <xxx@163.com>
2025-01-09 21:45:29 +0800

    fix

--- singbox_reddit.sh
+++ singbox_reddit.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
 # 检查并安装依赖项
 install_dependency() {
     if ! command -v "$1" >/dev/null 2>&1; then
